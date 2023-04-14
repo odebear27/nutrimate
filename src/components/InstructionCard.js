f4629b44e7376c1a0f98f1af7b5e25b9485b70ba
Leonard Lai <lailisheng@outlook.sg>
2023-04-14 22:41:46 +0800

    added recipe detail page updates

---
+++ src/components/InstructionCard.js
@@ -0,0 +1,53 @@
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBRow,
+    MDBCol,
+    MDBBtn
+  } from 'mdb-react-ui-kit';
+
+export default function InstructionCard({details}) {
+        console.log(details.analyzedInstructions);
+        const instructions = details.analyzedInstructions;
+
+
+            return(
+                
+                <div>          
+                    {/* {details.analyzedInstructions?.map((steps) => {  */}
+                        <MDBCard className="w-100 p-3">
+                            <MDBCardBody>
+                                <MDBCardTitle> Step 1</MDBCardTitle>
+                                    <MDBRow className='mt-3'>
+                                        <MDBCol sm='4' md='3' lg='4' xl='4'>
+                                            <MDBCard>
+                                                <MDBCardBody>
+                                                    <MDBCardTitle>Ingredient</MDBCardTitle>
+                                                    <MDBCardText>
+                                                        List of Ingredients
+                                                    </MDBCardText>
+                                                    
+                                                </MDBCardBody>
+                                            </MDBCard>
+                                        </MDBCol>
+                                        <MDBCol sm='6' md='3' lg='4' xl='6'>
+                                            <MDBCard>
+                                                <MDBCardBody>
+                                                    <MDBCardTitle>Method</MDBCardTitle>
+                                                    <MDBCardText>
+                                                        Detail Method
+                                                    </MDBCardText>
+                                                    
+                                                </MDBCardBody>
+                                            </MDBCard>
+                                        </MDBCol>
+                                    </MDBRow>
+                            </MDBCardBody>
+                        </MDBCard>
+                    {/* })} */}
+
+                </div>               
+            );        
+}
---
+++ src/pages/Recipe.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import NavbarHeader from '../components/Navbar';
+import Footer from '../components/Footer';
+import { MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import InstructionCard from '../components/InstructionCard';
+
+export default function Recipe() {
+
+    const[details, setDetails] = useState({});
+    let params = useParams()
+
+    var myHeaders = new Headers();
+      myHeaders.append("apikey", process.env.REACT_APP_API_KEY);
+      
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+    };
+
+    const fetchDetails = async (key) =>{
+        const data = await fetch(`https://api.apilayer.com/spoonacular/recipes/${key}/information?includeNutrition=false`, requestOptions);
+          
+        const detailData = await data.json();
+        console.log("detaildata",detailData);
+        setDetails(detailData)
+    }   
+
+    useEffect(()=>{
+        fetchDetails(params.key)
+    },[params.key])
+
+    return(
+        <>
+            <NavbarHeader/>
+
+            <section className=''>
+                <MDBContainer className='text-center text-md-start mt-5'>
+                    <MDBRow className='mt-3'>
+                        <MDBCol md='3' lg='4' xl='11' className='mx-auto mb-4'>
+                            <h1>{details.title}</h1>
+                        </MDBCol>
+                        
+                        <hr/>
+                        <MDBCol md='3' lg='4' xl='6' className='mx-auto mb-4'>
+                            <img src={details.image}/>
+                        </MDBCol>
+
+                        <MDBCol md='3' lg='4' xl='4' className='mx-auto mb-4'>
+                            <h6 className='text-uppercase fw-bold mb-4'>Infobox</h6>
+                            <p>
+                                <MDBIcon color='primary' fas icon='stopwatch' className='me-2' />
+                                
+                                Serve in <b>{details.readyInMinutes}</b> minutes
+                            </p>
+                            <p>
+                                <MDBIcon color='primary' fas icon="user-friends" className='me-2' />
+                                
+                                Serve <b>{details.servings}</b> pax
+                            </p>
+                            <p>
+                                <MDBIcon color='primary' fas icon="seedling" className='me-2' />
+                                
+                                Vegetarian? <b>{details.vegetarian === false ? 'No' : 'Yes'}</b>
+                            </p>
+                            <p>
+                                <MDBIcon color='primary' fas icon="dollar-sign" className='me-2' />
+                                Price per Serving : <b>$ {(details.pricePerServing)/ 100}</b>
+
+                            </p>
+                        </MDBCol>
+                        <hr/>
+                       
+                        <MDBCol md='3' lg='4' xl='11' className='mx-auto mb-4'>
+                            <p dangerouslySetInnerHTML={{__html:details.summary}} />
+                        </MDBCol>
+                        <hr/>
+                        <h3 className="text-center">Instruction</h3>
+                        <InstructionCard details={details}/>
+                    </MDBRow>
+                </MDBContainer>
+            </section>
+
+            <Footer />
+        </>
+    );
+}
